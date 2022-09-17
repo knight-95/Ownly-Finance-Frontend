91304682f252c978f9a237a2e52eba501b1d22a6
Aditya0049 <z87058556@gmail.com>
2022-09-17 23:39:46 +0530

    Solved-----Navbar contents are not aligned properly on desktop version.

--- src/components/Desktop1/Navbar.jsx
+++ src/components/Desktop1/Navbar.jsx
@@ -1,16 +1,23 @@
 import React from 'react'
 import {SiDiscord} from 'react-icons/si'
 import {BsTwitter} from 'react-icons/bs'
-import logo from "../../images/logo.png";
+import logo from "../../images/logo.png"
+import './Navbar.css'
 
 const Navbar = () => {
   return (
     <nav>
     <div className='navbar'>
     <img src={logo} alt="logo" width="150px" className='nav-logo'/>
+      <div className='nav-title'>
       <p>Join us</p>
+      </div>
+      <div className='nav-links'>
       <BsTwitter/>
-      <SiDiscord/>
+      </div> 
+      <div className='nav-discord'>
+      <SiDiscord/>    
+      </div>
     </div>
     </nav>
   )
